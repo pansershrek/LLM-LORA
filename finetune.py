5bb4aee2c11c9c8d496c8c8458fa28bb02d46a3f
Gleb Skiba <gleb-skiba@mail.ru>
2023-11-13 02:45:31 +0300

    Update

--- finetune.py
+++ finetune.py
@@ -6,53 +6,66 @@ import bitsandbytes as bnb
 from datasets import load_dataset
 import transformers
 
-assert (
-    "LlamaTokenizer" in transformers._import_structure["models.llama"]
-), "LLaMA is now in HuggingFace's main branch.\nPlease reinstall it: pip uninstall transformers && pip install git+https://github.com/huggingface/transformers.git"
-from transformers import LlamaForCausalLM, LlamaTokenizer
+#assert (
+#    "LlamaTokenizer" in transformers._import_structure["models.llama"]
+#), "LLaMA is now in HuggingFace's main branch.\nPlease reinstall it: pip uninstall transformers && pip install git+https://github.com/huggingface/transformers.git"
+#from transformers import LlamaForCausalLM, LlamaTokenizer
 from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer, BloomForCausalLM
 from transformers.models.opt.modeling_opt import OPTDecoderLayer
 
 from peft import (
-    prepare_model_for_int8_training,
     LoraConfig,
     get_peft_model,
     get_peft_model_state_dict,
 )
 
 
+import json
+
+import wandb
+from peft import (
+    LoraConfig, PeftConfig, PeftModel,
+    get_peft_model,
+    prepare_model_for_kbit_training
+)
+from transformers import (AutoModelForCausalLM, AutoTokenizer,
+                          DataCollatorForSeq2Seq,
+                          DataCollatorForTokenClassification, EvalPrediction,
+                          T5ForConditionalGeneration, Trainer, TrainerCallback,
+                          TrainerControl, TrainerState, TrainingArguments)
+from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
+
+
 # optimized for RTX 3090 and A100. for larger GPUs, increase some of these?
 MICRO_BATCH_SIZE = 4  # this could actually be 5 but i like powers of 2
-BATCH_SIZE = 128
+BATCH_SIZE = 32
 GRADIENT_ACCUMULATION_STEPS = BATCH_SIZE // MICRO_BATCH_SIZE
 EPOCHS = 3  # we don't always need 3 tbh
 LEARNING_RATE = 3e-4  # the Karpathy constant
-CUTOFF_LEN = 256  # 256 accounts for about 96% of the data
+CUTOFF_LEN = 512  # 256 accounts for about 96% of the data
 LORA_R = 8
 LORA_ALPHA = 16
 LORA_DROPOUT = 0.05
 VAL_SET_SIZE = 2000
 TARGET_MODULES = [
-    "q_proj",
-    "v_proj",
+    "q_proj", "k_proj", "v_proj", "o_proj"
 ]
-DATA_PATH = "alpaca_data_cleaned.json"
 
-model_name = "bigscience/bloom-560m"
+model_name = "mistralai/Mistral-7B-Instruct-v0.1"
 #model_name = "bigscience/bloom-1b1"
 #model_name = "bigscience/bloom-1b7"
 #model_name = "bigscience/bloom-3b"
 #model_name = "bigscience/bloom-7b1"
 #model_name = "bigscience/bloom" # for 176B parameters
 
-model = BloomForCausalLM.from_pretrained( 
+model = AutoModelForCausalLM.from_pretrained(
     model_name,
-    device_map='auto',
+    device_map='cuda:0',
     load_in_8bit=True,
 )
-tokenizer = AutoTokenizer.from_pretrained('bigscience/bloom')
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-model = prepare_model_for_int8_training(model)
+model = prepare_model_for_kbit_training(model)
 
 config = LoraConfig(
     r=LORA_R,
@@ -63,14 +76,14 @@ config = LoraConfig(
     task_type="CAUSAL_LM",
 )
 model = get_peft_model(model, config)
-tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos token
-data = load_dataset("json", data_files=DATA_PATH)
+#tokenizer.pad_token_id = 0  # unk. we want this to be different from the eos token
+#data = load_dataset("json", data_files=DATA_PATH)
 
-train_val = data["train"].train_test_split(
-    test_size=VAL_SET_SIZE, shuffle=True, seed=42
-)
-train_data = train_val["train"]
-val_data = train_val["test"]
+#train_val = data["train"].train_test_split(
+#    test_size=VAL_SET_SIZE, shuffle=True, seed=42
+#)
+train_data = load_dataset("json", data_files="train_data.json")
+val_data = load_dataset("json", data_files="val_data.json")
 
 
 def generate_prompt(data_point):
@@ -171,24 +184,28 @@ trainer = transformers.Trainer(
     train_dataset=train_data,
     eval_dataset=val_data,
     args=transformers.TrainingArguments(
+        report_to='wandb',
         per_device_train_batch_size=MICRO_BATCH_SIZE,
         gradient_accumulation_steps=GRADIENT_ACCUMULATION_STEPS,
         warmup_steps=100,
         num_train_epochs=EPOCHS,
         learning_rate=LEARNING_RATE,
         fp16=True,
-        logging_steps=20,
-        evaluation_strategy="steps",
-        save_strategy="steps",
+        logging_steps=1,
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
         eval_steps=200,
         save_steps=200,
-        output_dir="BLOOM-alpaca",
+        output_dir="mistral_finetune",
         save_total_limit=3,
         load_best_model_at_end=True,
         ddp_find_unused_parameters=False if ddp else None,
     ),
     data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
 )
+
+
+
 model.config.use_cache = False
 
 old_state_dict = model.state_dict
@@ -199,8 +216,11 @@ model.state_dict = (
 if torch.__version__ >= "2":
     model = torch.compile(model)
 
-trainer.train(resume_from_checkpoint = True) #if resume, choose True, else False
+with wandb.init(project="Instruction NER") as run:
+    model.print_trainable_parameters()
+    trainer.train() #if resume, choose True, else False
+    torch.save(model.state_dict(), f"final_mistral.pth")
 
-model.save_pretrained("BLOOM-alpaca")
+model.save_pretrained("mistral_finetune")
 
 print("\n If there's a warning about missing keys above, please disregard :)")
