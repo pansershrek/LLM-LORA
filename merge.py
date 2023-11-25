85c80849c15ece0e623dec0c309f1b081c04fb28
Gleb Skiba <g.skiba@tinkoff.ru>
2023-11-26 01:24:50 +0300

    MErge

--- merge.py
+++ merge.py
@@ -11,11 +11,11 @@ from transformers.models.opt.modeling_opt import OPTDecoderLayer
 
 model = AutoModelForCausalLM.from_pretrained(
     "mistralai/Mistral-7B-Instruct-v0.1",
-    load_in_8bit=True,
-    torch_dtype=torch.float16,
+    #load_in_8bit=True,
+    #torch_dtype=torch.float16,
     device_map='cuda:0'
 )
-model = prepare_model_for_kbit_training(model)
+#model = prepare_model_for_kbit_training(model)
 model = PeftModel.from_pretrained(model, "/home/admin/LLM-LORA/mistral_finetune_4")
 model = model.merge_and_unload()
 model.save_pretrained("/data/LLM-LORA/new_weights")
