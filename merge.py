455e6d6ddc6b5bf7e7af32082ddf8e96d678a5d8
Gleb Skiba <g.skiba@tinkoff.ru>
2023-11-26 01:32:02 +0300

    update

--- merge.py
+++ merge.py
@@ -15,7 +15,9 @@ model = AutoModelForCausalLM.from_pretrained(
     #torch_dtype=torch.float16,
     device_map='cpu'
 )
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1")
 #model = prepare_model_for_kbit_training(model)
 model = PeftModel.from_pretrained(model, "/home/admin/LLM-LORA/mistral_finetune_4")
 model = model.merge_and_unload()
 model.save_pretrained("/data/LLM-LORA/new_weights")
+tokenizer.save_pretrained("/data/LLM-LORA/new_weights")
