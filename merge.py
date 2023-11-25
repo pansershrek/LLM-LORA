d039bfa54172cbdafd57e3f53a1a7d3efe7d5b1e
Gleb Skiba <g.skiba@tinkoff.ru>
2023-11-26 01:28:03 +0300

    update

--- merge.py
+++ merge.py
@@ -13,7 +13,7 @@ model = AutoModelForCausalLM.from_pretrained(
     "mistralai/Mistral-7B-Instruct-v0.1",
     #load_in_8bit=True,
     #torch_dtype=torch.float16,
-    device_map='cuda:0'
+    device_map='cpu'
 )
 #model = prepare_model_for_kbit_training(model)
 model = PeftModel.from_pretrained(model, "/home/admin/LLM-LORA/mistral_finetune_4")
